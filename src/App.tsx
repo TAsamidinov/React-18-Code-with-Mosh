e0f5ae72ee0c34407644ca1cb196c65ec2e483f3
Temirlan <temirlan.asamidinov@gmail.com>
2025-11-17 17:28:02 -0600

    Use styled-components for ListGroup

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,24 @@
 import Alert from "./components/Alert";
 import Buttons from "./components/Buttons";
 import { useState } from "react";
+import ListGroup from "./components/ListGroup";
 
 function App() {
   const [showAlert, setShowAlert] = useState(false);
+  const items = ["New York", "San Francisco", "Tokyo", "London", "Berlin"];
 
   return (
     <div>
       {showAlert && (
-        <Alert onClose={() => setShowAlert(false)}>This is an alert!</Alert>
+        <Alert onClose={() => setShowAlert(false)}>Submited Succesfully!</Alert>
       )}
+      <ListGroup
+        items={items}
+        heading="Countries"
+        onSelectItem={(item) => console.log(item)}
+      />
       <Buttons color="primary" onClick={() => setShowAlert(true)}>
-        Primary
+        Submit
       </Buttons>
     </div>
   );
--- src/components/ListGroup.tsx
+++ src/components/ListGroup.tsx
@@ -1,11 +1,32 @@
 import { MouseEvent } from "react";
 import { useState } from "react";
+import styled from "styled-components";
 
+interface ListItemProps {
+  active: boolean;
+}
 interface Props {
   items: string[];
   heading: string;
   onSelectItem: (item: string) => void;
 }
+
+const List = styled.ul`
+  padding: 0;
+  margin: 0;
+  list-style: none;
+`;
+const ListItem = styled.li<ListItemProps>`
+  padding: 10px 15px;
+  border: 1px solid #ddd;
+  cursor: pointer;
+
+  &:hover {
+    background: #0d6efd;
+  }
+
+  background: ${(props) => (props.active ? '#0d6efd' : "none")};
+`;
 
 function ListGroup({ items, heading, onSelectItem }: Props) {
   const [selectedIndex, setSelectedIndex] = useState(-1);
@@ -21,14 +42,9 @@ function ListGroup({ items, heading, onSelectItem }: Props) {
   return (
     <>
       <h1> {heading} </h1>
-      <ul className="list-group">
+      <List>
         {items.map((item, index) => (
-          <li
-            className={
-              selectedIndex === index
-                ? "list-group-item active"
-                : "list-group-item"
-            }
+          <ListItem active
             key={item}
             onClick={() => {
               setSelectedIndex(index);
@@ -36,9 +52,9 @@ function ListGroup({ items, heading, onSelectItem }: Props) {
             }}
           >
             {item}
-          </li>
+          </ListItem>
         ))}
-      </ul>
+      </List>
     </>
   );
 }
