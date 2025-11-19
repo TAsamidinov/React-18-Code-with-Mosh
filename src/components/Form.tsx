043756fcff0af62d1dbe362af22a823fbcb1fc05
Temirlan <temirlan.asamidinov@gmail.com>
2025-11-19 02:45:19 -0600

    Create access to Inputs

--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -1,13 +1,32 @@
-import React from "react";
+import React, { FormEvent } from "react";
 
 const Form = () => {
+  const nameRef = React.useRef<HTMLInputElement>(null);
+  const ageRef = React.useRef<HTMLInputElement>(null);
+  const person = {
+    name: "",
+    age: 0,
+  };
+
+  const handleSubmit = (event: FormEvent) => {
+    event.preventDefault();
+
+    if (nameRef.current !== null) {
+      person.name = nameRef.current.value;
+    }
+    if (ageRef.current !== null) {
+      person.age = parseInt(ageRef.current.value);
+    }
+    console.log("Submitted person:", person);
+  };
   return (
-    <form>
+    <form onSubmit={handleSubmit}>
       <div className="mb-3">
         <label htmlFor="" className="form-label">
           Name
         </label>
         <input
+          ref={nameRef}
           id="name"
           type="text"
           className="form-control"
@@ -19,6 +38,7 @@ const Form = () => {
           Age
         </label>
         <input
+          ref={ageRef}
           id="age"
           type="number"
           className="form-control"
