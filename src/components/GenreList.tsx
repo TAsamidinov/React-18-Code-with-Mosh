eea5f04b38cdaeb3157fd55d977d26c9967e076d
Temirlan <temirlan.asamidinov@gmail.com>
2025-12-09 05:37:00 -0600

    Showing a Spinner

--- src/components/GenreList.tsx
+++ src/components/GenreList.tsx
@@ -1,24 +1,28 @@
 import { HStack, Image, List, ListIcon, ListItem, Text } from "@chakra-ui/react";
 import useGenres from "../hooks/useGenres";
 import getCroppedImageUrl from "../services/image-url";
+import { Spinner } from "@chakra-ui/react"
 
 const GenreList = () => {
-  const { data } = useGenres();
+  const { data, isLoading, error } = useGenres();
+
+  if (error) return null;
+  if (isLoading) return <Spinner />
 
   return (
     <List>
-      {data.map((genre) => (
-        <ListItem key={genre.id} paddingY={'8px'}>
-          <HStack>
-            <Image
-              boxSize={"32px"}
-              borderRadius={8}
-              src={getCroppedImageUrl(genre.image_background)}
-            />
-            <Text fontSize={'lg'}> {genre.name} </Text>
-          </HStack>
-        </ListItem>
-      ))}
+        {data.map((genre) => (
+            <ListItem key={genre.id} paddingY={'8px'}>
+            <HStack>
+                <Image
+                boxSize={"32px"}
+                borderRadius={8}
+                src={getCroppedImageUrl(genre.image_background)}
+                />
+                <Text fontSize={'lg'}> {genre.name} </Text>
+            </HStack>
+            </ListItem>
+        ))}
     </List>
   );
 };
