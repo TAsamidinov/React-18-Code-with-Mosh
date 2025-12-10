810cff4afc540b62091d3bd6b9aa27c2290c0662
Temirlan <temirlan.asamidinov@gmail.com>
2025-12-10 03:05:59 -0600

    Final Refactoring Game Cards

--- src/components/GameCard.tsx
+++ src/components/GameCard.tsx
@@ -1,31 +1,42 @@
-import React from 'react'
-import { Game } from '../hooks/useGames'
-import { Card, CardBody, HStack } from '@chakra-ui/react'
-import { Image } from '@chakra-ui/react'
-import { Heading } from '@chakra-ui/react'
-import { Text } from '@chakra-ui/react'
-import PlatformIconList from './PlatformIconList'
-import CriticScore from './CriticScore'
-import getCroppedImageUrl from '../services/image-url'
-import Emoji from './Emoji'
+import React from "react";
+import { Game } from "../hooks/useGames";
+import { Card, CardBody, HStack } from "@chakra-ui/react";
+import { Image } from "@chakra-ui/react";
+import { Heading } from "@chakra-ui/react";
+import { Text } from "@chakra-ui/react";
+import PlatformIconList from "./PlatformIconList";
+import CriticScore from "./CriticScore";
+import getCroppedImageUrl from "../services/image-url";
+import Emoji from "./Emoji";
 
 interface GameCardProps {
-    game: Game,
+  game: Game;
 }
 
-const GameCard = ({game}: GameCardProps) => {
+const GameCard = ({ game }: GameCardProps) => {
   return (
-    <Card >
-        <Image src={ getCroppedImageUrl(game.background_image) } />
-        <CardBody >
-            <HStack marginBottom={3} justifyContent={'space-between'}>
-                <PlatformIconList platforms={game.parent_platforms.map(p => p.platform)} />
-                <CriticScore score={game.metacritic}  />
-            </HStack>
-            <Heading fontSize='2xl'>{ game.name } <Emoji rating={game.rating_top} /></Heading>
-        </CardBody>
+    <Card>
+      <Image src={getCroppedImageUrl(game.background_image)} />
+      <CardBody>
+        <HStack marginBottom={3} justifyContent={"space-between"}>
+          <PlatformIconList
+            platforms={game.parent_platforms.map((p) => p.platform)}
+          />
+          <CriticScore score={game.metacritic} />
+        </HStack>
+        <HStack justifyContent="space-between" alignItems="start">
+          <Heading
+            fontSize="2xl"
+            noOfLines={2}
+            minHeight="3.6em" // ✅ Reserves space for exactly 2 lines
+          >
+            {game.name}
+          </Heading>
+          <Emoji rating={game.rating_top} />
+        </HStack>
+      </CardBody>
     </Card>
-  )
-}
+  );
+};
 
-export default GameCard
+export default GameCard;
